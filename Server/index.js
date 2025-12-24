b4ed725bf6befca71dae3be59bab06aedc9f328e
Tanmay s ail <142146589+HellTanmay@users.noreply.github.com>
2025-12-24 16:44:23 +0530

    Few Changes2

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ config({ path: "./.env" });
 
 
 var corsOptions = {
-  origin: ['http://localhost:3000','https://skillbites.netlify.app,','https://skillbites-backend.onrender.com'],
+  origin: ['http://localhost:3000','https://skillbites.netlify.app,','https://skillbites-frontend.onrender.com'],
   credentials:true
 }
 app.use(cookieParser());
